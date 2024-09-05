9a9ec3ea3d3ebefedac9fbbe8418e78e126a2dec
Dmytro Klepachevskyi <dima@Dmytros-MacBook-Pro.local>
2024-09-05 13:12:34 +0300

    Initial + layout

---
+++ experiments/opencv.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+
+image = np.zeros((400, 600, 3), dtype=np.uint8)
+
+cv2.rectangle(image, (50, 50), (200, 150), (0, 255, 0), thickness=2)
+
+cv2.circle(image, (300, 100), 30, (0, 0, 255), thickness=-1)
+
+cv2.imshow('Opencv example', image)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
